9ba963bcb41e78b68b5ecfa10d797a96c04f1c83
Thiago Luis <thiagoluisrpinho@gmail.com>
2020-02-20 19:08:27 -0300

    :sparkles: feat: both functions now working for multiple documents

--- text_classification/preprocessing.py
+++ text_classification/preprocessing.py
@@ -2,73 +2,85 @@ import pdftotext
 import os
 import re
 import pandas as pd
-from typing import List
+from typing import List, Tuple
 
 DATASETS_RELATIVE_PATH = "./assets/datasets/"
 
 
-def open_document(
-        filename: str, foldername: str = DATASETS_RELATIVE_PATH) -> List[str]:
+def open_documents(
+        filenames: List[str],
+        foldername: str = DATASETS_RELATIVE_PATH) -> List[Tuple[str, List[str]]]:
     '''
-    Open a pdf document alterady OCRized, stores a copy of the content as
-    a text file(.txt) inside a folder "./txts/" in the same directory as
-    the original pdf
-    :args: filename -> string, foldername -> string
-    :return: document_pages -> List[string]
+    Open multiple pdf documents alterady OCRized, stores a copy of their
+    contents as a text file(.txt) inside a folder "./txts/" in the same
+    directory as the original pdf
+    :args: filenames -> a list containing each pdf filename
+            foldername -> a string containing the folder with the documents
+                relative path
+    :return: document_pages -> List of tuples with filenames and their
+            respective pages
     '''
-    if filename == "txts":
-        ''' Stops the function from opening folder as pdf '''
+    if not filenames:
         return []
+    documents = []
+    for filename in filenames:
+        if filename == "txts":
+            ''' Stops the function from opening folder as pdf '''
+            continue
 
-    filename = re.sub(".pdf", "", filename)
-    if not os.path.isfile(foldername + filename + ".pdf"):
-        return []
+        filename = re.sub(".pdf", "", filename)
+        if not os.path.isfile(foldername + filename + ".pdf"):
+            continue
 
-    if not os.path.exists(foldername + "txts/"):
-        os.makedirs(foldername + "txts/")
+        if not os.path.exists(foldername + "txts/"):
+            os.makedirs(foldername + "txts/")
 
-    text_filename = foldername + "txts/" + filename + ".txt"
-    end_of_page_token = "endofpagemarksymbol"
+        text_filename = foldername + "txts/" + filename + ".txt"
+        end_of_page_token = "endofpagemarksymbol"
 
-    if not os.path.exists(text_filename):
-        ''' Only reads the pdf if a quicker txt version is not available '''
-        with open(foldername + filename + ".pdf", "rb") as pdf_file:
-            untreated_document = pdftotext.PDF(pdf_file)
-            document_pages = []
+        if not os.path.exists(text_filename):
+            ''' Only reads the pdf if a quicker txt version is not available '''
+            with open(foldername + filename + ".pdf", "rb") as pdf_file:
+                untreated_document = pdftotext.PDF(pdf_file)
+                document_pages = []
 
-        with open(text_filename, "w") as text_file:
-            for page in untreated_document:
-                print(page.upper() + end_of_page_token, file=text_file)
-                document_pages.append(page.upper())
-    else:
-        with open(text_filename, "r") as text_file:
-            full_text_content = text_file.read()
-            document_pages = full_text_content.split(end_of_page_token)
-            # The split function is returning an empty blank page at end of
-            # the list so we have to drop it
-            document_pages.pop()
+            with open(text_filename, "w") as text_file:
+                for page in untreated_document:
+                    print(page.upper() + end_of_page_token, file=text_file)
+                    document_pages.append(page.upper())
+        else:
+            with open(text_filename, "r") as text_file:
+                full_text_content = text_file.read()
+                document_pages = full_text_content.split(end_of_page_token)
+                # The split function is returning an empty blank page at end of
+                # the list so we have to drop it
+                document_pages.pop()
+        documents.append((filename, document_pages))
 
-    return document_pages
+    return documents
 
 
-def document_pages_to_dataframe(document_pages: List[str], document_name: str):
-    ''' Receives a list of document_pages and convert the pages of
-        each document in a row with the  content of the page and
-        the document identifier as columns.
-        :args: 
+def documents_pages_to_dataframe(documents: List[Tuple[str, List[str]]]):
+    ''' Receives a list of documents with filenames a pages
+        and convert the pages of each page in a row with the 
+        content of the page and the document identifier as columns.
+        If any of the arguments is empty or they're not
+        the same size the function will return an empty dataframe
+        :args: documents -> Multiple documents with multiple pages
         :returns: dataframe containing all pages as rows
     '''
-    if not document_pages or not document_name:
+    if not documents:
         return pd.DataFrame()
 
-    document_pages_dicts = []
-    for page in document_pages:
-        single_page_dict = {
-            "page_content": page,
-            "filename": document_name
-        }
-        document_pages_dicts.append(single_page_dict)
+    documents_dicts = []
+    for document_name, pages in documents:
+        for page in pages:
+            single_page_dict = {
+                "page_content": page,
+                "filename": document_name
+            }
+            documents_dicts.append(single_page_dict)
 
-    df_document_pages = pd.DataFrame(document_pages_dicts)
+    df_documents_pages = pd.DataFrame(documents_dicts)
 
-    return df_document_pages
+    return df_documents_pages
--- text_classification/tests/tests_preprocessing.py
+++ text_classification/tests/tests_preprocessing.py
@@ -8,7 +8,7 @@
 
 import pytest
 import helper_tests
-from preprocessing import open_document, document_pages_to_dataframe
+from preprocessing import open_documents, documents_pages_to_dataframe
 
 
 @pytest.mark.parametrize('input_and_output', [
@@ -20,13 +20,13 @@ from preprocessing import open_document, document_pages_to_dataframe
 def test_pdf_to_text(input_and_output):
     filename = input_and_output[0]
     expected_output = input_and_output[1]
-    extracted_string = open_document(
+    extracted_string = open_documents(
         filename, helper_tests.TESTS_SAMPLES_RELATIVE_PATH)
     found_page = len(extracted_string) > 0
 
     assert expected_output == found_page
     ''' Then we test if the text file importation is having any problem '''
-    extracted_string = open_document(
+    extracted_string = open_documents(
         filename, helper_tests.TESTS_SAMPLES_RELATIVE_PATH)
 
     assert expected_output == found_page
@@ -37,16 +37,14 @@ def test_pdf_to_text(input_and_output):
     (["45198473.pdf", "48276987.pdf"], True),
     (["45198473.pdf", "invalido.pdf"], True),
     (["48276987.pdf"], True)])
-def test_document_pages_to_dataframe(input_and_output):
+def test_documents_pages_to_dataframe(input_and_output):
     input_filename = input_and_output[0]
     expected_output = input_and_output[1]
-    document_pages = open_document(
+    documents = open_documents(
         input_filename, helper_tests.TESTS_SAMPLES_RELATIVE_PATH)
 
-    df_document_pages = document_pages_to_dataframe(
-        document_pages, input_filename)
-    
+    df_document_pages = documents_pages_to_dataframe(documents)
+
     is_dataframe = not df_document_pages.empty
 
     assert expected_output == is_dataframe
-
